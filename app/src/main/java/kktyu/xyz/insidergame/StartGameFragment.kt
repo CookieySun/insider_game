85297c8033f418672429b09a58924dce343eee0a
kktyu <ku.k669500@gmail.com>
2020-01-01 16:12:18 +0900

    StartGameFragment作成

---
+++ app/src/main/java/kktyu/xyz/insidergame/StartGameFragment.kt
@@ -0,0 +1,28 @@
+package kktyu.xyz.insidergame
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kktyu.xyz.insidergame.databinding.FragmentStartGameBinding
+
+class StartGameFragment : Fragment() {
+    lateinit var binding: FragmentStartGameBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentStartGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.startGameBtn.setOnClickListener {
+
+        }
+    }
+}
