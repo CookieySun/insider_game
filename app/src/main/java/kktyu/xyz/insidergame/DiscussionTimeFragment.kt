2408e888473dd2fcfa2eea01cd9562f5f907406a
kktyu <ku.k669500@gmail.com>
2020-02-11 23:00:25 +0900

    議論タイム画面作成

--- app/src/main/java/kktyu/xyz/insidergame/DiscussionTimeFragment.kt
+++ app/src/main/java/kktyu/xyz/insidergame/DiscussionTimeFragment.kt
@@ -3,16 +3,33 @@ package kktyu.xyz.insidergame
 
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isGone
+import android.view.animation.AlphaAnimation
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import kktyu.xyz.insidergame.databinding.FragmentCountDownBinding
 
+
 class DiscussionTimeFragment : Fragment() {
     lateinit var binding: FragmentCountDownBinding
+
+    private val timer by lazy {
+        object : CountDownTimer(300000, 250) {
+            override fun onTick(millisUntilFinished: Long) {
+                val millisSec = millisUntilFinished / 1000
+                val sec = "%02d".format(millisSec % 60)
+                val min = millisSec / 60
+
+                binding.remainingTime = "$min:$sec"
+            }
+
+            override fun onFinish() {
+            }
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,22 +41,55 @@ class DiscussionTimeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.answerButton.isGone = true
+        var visible = false
+
+        binding.answerButton.text = this.getText(R.string.discussion_finished)
         binding.questionTimeText.text = this.getText(R.string.discussion_time)
-
-        val timer = object : CountDownTimer(300000, 250) {
-            override fun onTick(millisUntilFinished: Long) {
-                val millisSec = millisUntilFinished / 1000
-                val sec = "%02d".format(millisSec % 60)
-                val min = millisSec / 60
-
-                binding.remainingTime = "$min:$sec"
-            }
-
-            override fun onFinish() {
-                Log.d("loglog", "onFinish")
+        binding.helpButton.apply {
+            isVisible = true
+            setOnClickListener {
+                binding.descriptionText.isVisible = true
+                if (!visible) {
+                    fadein()
+                    visible = !visible
+                }
             }
         }
+
+        binding.descriptionText.text = this.getText(R.string.description_discussion)
+
+        binding.parentView.setOnClickListener {
+            if (visible) {
+                fadeout()
+                visible = !visible
+            }
+        }
+
         timer.start()
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        timer.cancel()
+    }
+
+    private fun fadeout() { // 透明度を1から0に変化
+        val alphaFadeout = AlphaAnimation(1.0f, 0.0f)
+        // animation時間 msec
+        alphaFadeout.duration = 500
+        // animationが終わったそのまま表示にする
+        alphaFadeout.fillAfter = true
+        binding.descriptionText.startAnimation(alphaFadeout)
+    }
+
+    private fun fadein() { // 透明度を0から1に変化
+        val alphaFadeIn = AlphaAnimation(0.0f, 1.0f)
+        // animation時間 msec
+        alphaFadeIn.duration = 500
+        // animationが終わったそのまま表示にする
+        alphaFadeIn.fillAfter = true
+        binding.descriptionText.startAnimation(alphaFadeIn)
     }
 }
