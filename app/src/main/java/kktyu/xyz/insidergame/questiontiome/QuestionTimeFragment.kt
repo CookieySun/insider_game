a36c92c1c377cde148f99350e52754e6d1df7948
kktyu <ku.k669500@gmail.com>
2020-03-28 17:46:31 +0900

    エラーを解消

--- app/src/main/java/kktyu/xyz/insidergame/questiontiome/QuestionTimeFragment.kt
+++ app/src/main/java/kktyu/xyz/insidergame/questiontiome/QuestionTimeFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import kktyu.xyz.insidergame.QuestionTimeFragmentDirections
 import kktyu.xyz.insidergame.databinding.FragmentCountDownBinding
 
 class QuestionTimeFragment : Fragment() {
--- app/src/main/java/kktyu/xyz/insidergame/startgame/StartGameFragment.kt
+++ app/src/main/java/kktyu/xyz/insidergame/startgame/StartGameFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import kktyu.xyz.insidergame.MainActivity
-import kktyu.xyz.insidergame.StartGameFragmentDirections
 import kktyu.xyz.insidergame.databinding.FragmentStartGameBinding
 
 class StartGameFragment : Fragment() {
